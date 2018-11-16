a09e3c670d60d9ae18395ca396ecfc1b86e95584
stuart gill <stuart.a.gill@gmail.com>
2018-11-16 11:27:53 -0800

    create functioning word counter method and test for it

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -6,33 +6,44 @@ namespace WordCounter.Tests
   [TestClass]
   public class RepeatCounterTest
   {
-
     [TestMethod]
     public void RepeatCounterConstructor_CreatesInstanceofRepeatCounter_RepeatCounter()
     {
-      RepeatCounter newRepeatCounter = new RepeatCounter("test");
+      RepeatCounter newRepeatCounter = new RepeatCounter("test sentence");
       Assert.AreEqual(typeof(RepeatCounter), newRepeatCounter.GetType());
     }
 
     [TestMethod]
-    public void GetWord_ReturnsWord_String()
+    public void GetSentence_ReturnsSentence_String()
     {
-      string testWord = "Snarfle";
-      RepeatCounter newRepeatCounter = new RepeatCounter(testWord);
-      string result = newRepeatCounter.GetWord();
-      Assert.AreEqual(testWord, result);
+      string testSentence = "Snarfle";
+      RepeatCounter newRepeatCounter = new RepeatCounter(testSentence);
+      string result = newRepeatCounter.GetSentence();
+      Assert.AreEqual(testSentence, result);
     }
 
     [TestMethod]
-    public void SetWord_SetsWord_String()
+    public void SetSentence_SetsSentence_String()
     {
-      string testWord = "Barfle";
-      RepeatCounter newRepeatCounter = new RepeatCounter(testWord);
-      string updatedTestWord = "Snarfle";
-      newRepeatCounter.SetWord(updatedTestWord);
-      string result = newRepeatCounter.GetWord();
-      Assert.AreEqual(updatedTestWord, result);
+      string testSentence = "Barfle Snarfle";
+      RepeatCounter newRepeatCounter = new RepeatCounter(testSentence);
+      string updatedTestSentence = "Snarfle Barfle";
+      newRepeatCounter.SetSentence(updatedTestSentence);
+      string result = newRepeatCounter.GetSentence();
+      Assert.AreEqual(updatedTestSentence, result);
     }
+
+    [TestMethod]
+    public void CountWordInSentence_CountsWordsInSentence_Int()
+    {
+      string testWord = "tarfle";
+      string testSentence = "barfle snarfle tarfle darfle tarfle";
+      RepeatCounter newRepeatCounter = new RepeatCounter(testSentence);
+      newRepeatCounter.CountWordInSentence(testWord);
+      int result = newRepeatCounter.CountWordInSentence(testWord);
+      Assert.AreEqual(2, result);
+    }
+
 
   }
 }
